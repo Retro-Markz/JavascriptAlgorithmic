63094beefb4931f95d940aea66695da663e163c1
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-08-14 02:02:04 -0600

    update

--- Basics/palindrome.js
+++ Basics/palindrome.js
@@ -1,6 +1,30 @@
 // "eye"
 //
 
-function palindrome(str) {}
+function palindrome(str) {
+  let front = 0;
+  let back = str.length - 1;
 
-console.log("eye");
+  while (back > front) {
+    //increment
+    while (str[front].match(/[\W_]/)) {
+      front++;
+      continue;
+    }
+    //decrement
+    while (str[back].match(/[\W_]/)) {
+      back--;
+      continue;
+    }
+    //do the comparison
+    if (str[front].toLowerCase() !== str[back].toLowerCase()) return false;
+    front++;
+    back--;
+  }
+  //if fralse. then is a palindrome
+  return true;
+}
+
+console.log(palindrome("eye"));
+console.log(palindrome("salas"));
+console.log(palindrome("casa"));
---
+++ Basics/titleCase.js
@@ -0,0 +1,7 @@
+//basic solution
+
+String.prototype.replaceAt = function (index, character) {};
+
+function titleCase() {}
+
+console.log(titleCase("I'm a little tea pot"));
