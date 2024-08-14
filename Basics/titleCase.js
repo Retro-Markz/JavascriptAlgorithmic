d14f074fd99312a2dbe8bdab1c5ee117209304ac
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-08-14 02:43:03 -0600

    title case

--- Basics/titleCase.js
+++ Basics/titleCase.js
@@ -1,7 +1,51 @@
 //basic solution
 
-String.prototype.replaceAt = function (index, character) {};
+/*
+String.prototype.replaceAt = function (index, character) {
+  return (
+    this.substr(0, index) + character + this.substr(index + character.length)
+  );
+};
 
-function titleCase() {}
+function titleCase(str) {
+  var newTitle = str.split(" ");
+  var updatedTitle = [];
+  for (var st in newTitle) {
+    updatedTitle[st] = newTitle[st]
+      .toLowerCase()
+      .replaceAt(0, newTitle[st].charAt(0).toUpperCase());
+  }
+  return updatedTitle.join(" ");
+}
 
+//intermediate solution
+
+function titleCase(str) {
+  var convertToArray = str.toLowerCase().split(" ");
+  var result = convertToArray.map(function (val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return result.join(" ");
+}
+
+//advanced solution
+
+function titleCase(str) {
+  return str.toLowerCase().replace(/(^|\s)\S/g, (L) => L.toUpperCase());
+}
+
+*/
+
+//advanced 2
+
+function titleCase(str) {
+  str = str.split(" ");
+
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].toLowerCase().split("");
+    str[i][0] = str[i][0].toUpperCase();
+    str[i] = str[i].join("");
+  }
+  return str.join(" ");
+}
 console.log(titleCase("I'm a little tea pot"));
