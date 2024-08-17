2afd27709badd6a9a6d545aa3732d8f79b37f613
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-08-17 06:30:26 -0600

    extending

--- BigO/bigO.js
+++ BigO/bigO.js
@@ -1,5 +1,6 @@
 //time perfonrmance
 
+/*
 function addUpTo(n) {
   return (n * (n + 1)) / 2;
 }
@@ -21,3 +22,23 @@ let t3 = performance.now();
 addUpTo2(1000);
 let t4 = performance.now();
 console.log(`time elapsed: ${(t4 - t3) / 1000} seconds`);
+
+
+function countUpToDown(n) {
+  console.log("going Up!");
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  console.log("At the top! \nGoing Down...");
+  for (let j = n - 1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log("Back down, Bye!");
+}
+
+let t1 = performance.now();
+console.log(countUpToDown(10));
+let t2 = performance.now();
+console.log(`time elapsed: ${(t2 - t1) / 1000} seconds`);
+
+*/
