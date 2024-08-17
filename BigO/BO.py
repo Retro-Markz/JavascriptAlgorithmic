ae06579e78b36d9fd812c6b750eefd1cad7a514b
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-08-17 05:42:00 -0600

    bigO n^2

--- BigO/BO.py
+++ BigO/BO.py
@@ -1,11 +1,13 @@
 ''''
+#O(n)
+
 def print_items(n):
     for i in range(n):
         print(i)
         
         
 print_items(10)
-'''
+
 
 def print_items(n):
     for i in range(n):
@@ -15,3 +17,16 @@ def print_items(n):
         print(j)
         
 print_items(10)
+
+#O(n^2)
+
+def print_items(n):
+    for i in range(n):
+       for j in range(n):
+           for k in range(n):
+                print(i,j,k)
+        
+        
+print_items(10)
+   
+'''
