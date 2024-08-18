e8f6fa2d3891cadf4edb6f6dbc0fa0ca9457d8fe
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-08-18 08:19:22 -0600

    char count refacotr

--- Basics_explained/charCount.js
+++ Basics_explained/charCount.js
@@ -1,5 +1,6 @@
 //make a function that cunt how many times a char repeats in a string, make ir alphanumeric only
 
+/*
 function charCount(str) {
   var obj = {};
   for (var i = 0; i < str.length; i++) {
@@ -15,7 +16,7 @@ function charCount(str) {
   return obj;
 }
 
-console.log(charCount("hello World, here i come"));
+*/
 
 //create a function that recives a string
 //create an empry array
@@ -24,3 +25,41 @@ console.log(charCount("hello World, here i come"));
 //the logic goes, if the char is > than 0 then increment
 //else, if the char is just 1, mantain in 1
 //return the obj
+
+//refactor, not for performance but for style
+
+function charCount(str) {
+  var obj = {};
+  for (var char of str) {
+    char = char.toLowerCase();
+    if (isAlphaNumeric(char)) {
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
+
+//in order to check performance, can use alphanumeric instead of regular expresion (regexp), but cheking out, it works
+//diferent in diferent enviroments, so, it's a good idea to check perfomrmance in the particular case of use for
+//this refactor
+
+//add this function and then replace the function for the regExp en the original func
+
+function isAlphaNumeric(str) {
+  for (let i = 0; i < str.length; i++) {
+    let code = str.charCodeAt(i);
+    if (
+      !(code > 47 && code < 58) && // numeric (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123)
+    ) {
+      // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isAlphaNumeric("hello"));
+
+console.log(charCount("hello World, here i come"));
