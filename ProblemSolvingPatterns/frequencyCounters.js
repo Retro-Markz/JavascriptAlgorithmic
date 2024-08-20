22bda98bac05d7128a3f4fa2785791afeb5f2ef7
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-08-20 00:41:04 -0600

    update frecuency counter

--- ProblemSolvingPatterns/frequencyCounters.js
+++ ProblemSolvingPatterns/frequencyCounters.js
@@ -28,9 +28,12 @@ function same(arr1, arr2) {
   return true;
 }
 
+let t1 = performance.now();
 console.log(same([1, 2, 3], [1, 4, 9]));
-console.log(same([1, 4, 3], [1, 4, 9]));
-console.log(same([1, 2, 3, 1], [1, 4, 1, 9]));
+let t2 = performance.now();
+console.log(`time elapsed: ${(t2 - t1) / 1000} seconds`);
+
+//time elapsed in proces is about 0.0013~0.009
 
 //refactores for a mor destructured solution but with O(n) complexity
 
@@ -38,6 +41,30 @@ function same2(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  let frewuency1 = {};
-  let frewuency2 = {};
+  let frequency1 = {};
+  let frequency2 = {};
+  for (let val of arr1) {
+    frequency1[val] = (frequency1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequency2[val] = (frequency2[val] || 0) + 1;
+  }
+  for (let key in frequency1) {
+    if (!(key ** 2 in frequency2)) {
+      return false;
+    }
+    if (frequency2[key ** 2] !== frequency1[key]) {
+      return false;
+    }
+  }
+  return true;
 }
+
+let t3 = performance.now();
+console.log(same2([1, 2, 3], [1, 4, 9]));
+let t4 = performance.now();
+console.log(`time elapsed: ${(t4 - t3) / 1000} seconds`);
+
+//so, the second is not that 'fancy' but time elapsed is 0.00015~0.00023
+
+//the goal  for me is to improve code performance
