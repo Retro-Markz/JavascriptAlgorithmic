7dd19bebe1b8775606bd3a5ad071912d4fcb97dc
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-09-01 07:21:35 -0600

    update

--- Recursion/nFactorial.js
+++ Recursion/nFactorial.js
@@ -1,3 +1,5 @@
+//factorial iteratively
+
 function nFactrial(num) {
   let total = 1;
   for (let i = num; i > 0; i--) {
@@ -6,4 +8,13 @@ function nFactrial(num) {
   return total;
 }
 
-console.log(nFactrial(3));
+console.log(nFactrial(4));
+
+//factorial recursively
+
+function factorial(numb) {
+  if (numb === 1) return 1;
+  return numb * factorial(numb - 1);
+}
+
+console.log(factorial(4));
